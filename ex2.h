6067b56743fb1bac000b40143259525d213ee5ae
Snir David Nahari <snirdavid1994@gmail.com>
2020-11-30 09:03:54 +0200

    adding files

---
+++ ex2.h
@@ -0,0 +1,15 @@
+//
+// Created by snira on 28/11/2020.
+//
+
+#ifndef COMPUTER_STRUCTURE_EX2_EX2_H
+#define COMPUTER_STRUCTURE_EX2_EX2_H
+
+
+void twoArgs(char *oldFileName, char *newFileName);
+void fourArgs(char *oldFileName, char *newFileName, char* srcOsFlag, char* destOsFlag);
+void fiveArgs(char *oldFileName, char *newFileName, char* srcOsFlag, char* destOsFlag, char* byteOrderFlag);
+
+
+
+#endif //COMPUTER_STRUCTURE_EX2_EX2_H
